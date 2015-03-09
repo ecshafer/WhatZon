f07b05310f25425f98458ebb27c582edb36ff30d
Matthew-Tebaldi <mtebaldi2@gmail.com>
2015-03-09 15:48:18 -0400

    link activity
    Link from main to login page

--- WhatzOn/app/src/main/java/com/andapp/csc380/whatzon/MainActivity.java
+++ WhatzOn/app/src/main/java/com/andapp/csc380/whatzon/MainActivity.java
@@ -1,24 +1,37 @@
 package com.andapp.csc380.whatzon;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
-public class EventDetail extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity {
+
+    public void toPageLogin(View view) {
+        Intent intent = new Intent(this, Login.class);
+       // EditText editText = (EditText) findViewById(R.id.edit_message);
+      //  String message = editText.getText().toString();
+      //  intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event_detail);
+        setContentView(R.layout.activity_main);
+
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_event_detail, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
