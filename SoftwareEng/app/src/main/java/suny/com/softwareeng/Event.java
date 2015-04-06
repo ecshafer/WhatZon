2f3c129f2493a980532ab5e858712fcdda170be4
saraiva3 <lucassaraiva12@yahoo.com.br>
2015-04-05 22:25:01 -0400

    Att

--- SoftwareEng/app/src/main/java/suny/com/softwareeng/Event.java
+++ SoftwareEng/app/src/main/java/suny/com/softwareeng/Event.java
@@ -12,17 +12,40 @@ import java.util.Date;
 public class Event {
 
     private String picture;
-    private String name;
-    private String address;
+    private String title;
+    private String location;
     private Date date;
     private Time time;
     private String tags;
+    private String moreInfo;
 
+    public Event( String title,String location,  String moreInfo){
 
-    public Event(String picture, String name){
-        this.picture =picture;
-        this.name = name;
+        this.title = title;
+        this.location = location;
+        this.moreInfo = moreInfo;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public String getLocation() throws IllegalStateException {
+        return location;
+    }
+    public void setMoreInfo(String moreInfo) {
+        this.moreInfo = moreInfo;
+    }
+    public String getMoreInfo() throws IllegalStateException {
+        return moreInfo;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getTitle() throws IllegalStateException {
+        return title;
     }
 
-
+    @Override
+    public String toString() {
+        return "Title:"+getTitle()+"\nLocation:"+getLocation()+"\nInformation"+getMoreInfo();
+    }
 }
