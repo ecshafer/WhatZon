f442d67d2886c5cbb68130c104276bc27112bb46
saraiva3 <lucassaraiva12@yahoo.com.br>
2015-04-08 18:32:21 -0400

    Update AddEvent.java

--- SoftwareEng/app/src/main/java/suny/com/softwareeng/AddEvent.java
+++ SoftwareEng/app/src/main/java/suny/com/softwareeng/AddEvent.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -20,35 +21,42 @@ import java.util.Date;
  * Created by Lucas on 3/25/2015.
  */
 public class AddEvent extends Activity{
-    ArrayList<Event> event = new ArrayList<Event>();
+
+    ArrayList<Event> event = new ArrayList<>();
     EditText title;
     EditText location;
     EditText time;
     EditText date;
-    ImageButton ok;
+    ImageButton ok,no;
+    Tags tags;
     EditText moreInfo;
     Event ev;
-
+    CheckBox musCheck,partCheck,movCheck,eduCheck,politCheck;
     String titleStr, locationStr,moreInfoStr, dateStr,timeStr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-
+        tags = new Tags(false,false,false,false,false);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-                super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.addevent);
-        ImageButton ok = (ImageButton) findViewById(R.id.btnYes);
-
+        ok = (ImageButton) findViewById(R.id.btnYes);
+        no = (ImageButton) findViewById(R.id.btnNo);
         final Context context = this;
-        //Remove notification bar
+        musCheck =(CheckBox) findViewById(R.id.musicBox);
+        partCheck =(CheckBox) findViewById(R.id.partyBox);
+        movCheck= (CheckBox) findViewById(R.id.moviesBox);
+        eduCheck =(CheckBox) findViewById(R.id.educationBox);
+        politCheck =(CheckBox) findViewById(R.id.politcsBox);
         title = (EditText) findViewById(R.id.Title);
         date = (EditText) findViewById(R.id.Date);
         time = (EditText) findViewById(R.id.Time);
         moreInfo = (EditText) findViewById(R.id.MoreInfo);
         location = (EditText) findViewById(R.id.Location);
 
+        String test = Login.name;
+        Log.d("NAME",test);
         ok.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if(title.getText().toString().trim().length() != 0)
@@ -58,25 +66,50 @@ public class AddEvent extends Activity{
                 if(moreInfo.getText().toString().trim().length() != 0)
                     moreInfoStr = moreInfo.getText().toString();
                 if(date.getText().toString().trim().length() != 0)
-                    dateStr = moreInfo.getText().toString();
+                    dateStr = date.getText().toString();
+
                 if(time.getText().toString().trim().length() != 0)
-                    timeStr = moreInfo.getText().toString();
-                ev = new Event(titleStr,locationStr,moreInfoStr,dateStr,timeStr);
+                    timeStr = time.getText().toString();
+               // Log.d("TEST",dateStr);
+                tags.setMusicTg(musCheck.isChecked());
+                tags.setPartyTg(partCheck.isChecked());
+                tags.setMovieTg(movCheck.isChecked());
+                tags.setEducationTg(eduCheck.isChecked());
+                tags.setPolitcsTg(politCheck.isChecked());
+
+
+                ev = new Event(titleStr,locationStr,moreInfoStr,dateStr,timeStr,tags);
                 event.add(ev);
                 Intent intent = new Intent(context, MenuPage.class);
                 startActivity(intent);
              //   Log.d("Events",event.get(0).getTitle());
+                Log.d("TAGS",tags.toString());
                 Log.isLoggable("Events",event.size());
                 for(int i=0;i<event.size();i++){
                     Log.d("Events",event.get(i).toString());
                 }
             }
         });
-
+        no.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(title.getText().toString().trim().length() != 0)
+                 title.setText("");
+                if(location.getText().toString().trim().length() != 0)
+                    location.setText("");
+                if(moreInfo.getText().toString().trim().length() != 0)
+                    moreInfo.setText("");
+                if(date.getText().toString().trim().length() != 0){
+                    date.setText("");
+                if(time.getText().toString().trim().length() != 0)
+                     time.setText("");
+                }
+            }
+        });
         ImageButton menu = (ImageButton) findViewById(R.id.menuBtn);
         menu.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(context, MenuPage.class);
+                Intent intent = new Intent(context, Events.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
             }
         });
